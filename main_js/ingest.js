bb7ccc9b550ac718ceb57df1024aa27d7808bd99
pereznancy <nan.per9@outlook.com>
2018-03-14 20:53:58 -0700

    ingest arrays made

--- main_js/ingest.js
+++ main_js/ingest.js
@@ -23,14 +23,14 @@ var foodArray = [
 
 
 var drinkArray = [
-  ("Pearl Tavern", "images/ingest/pearl.jpg","http://pearltavernpdx.com"),
-  ("The Old Portland", "images/ingest/old.jpg", "http://pearltavernpdx.com"),
-  ("Bar Casa Vale", "images/ingest/vale.jpg", "www.barcasavale.com"),
-  ("Century", "images/ingest/century.jpg", "http://centurybarpdx.com/"),
-  ("The Bye and Bye", "images/ingest/bye.jpg", "http://thebyeandbye.com"),
-  ("Capitol", "images/ingest/capitol.jpg", "http://capitolpdx.com/"),
-  ("Dig A Pony", "images/ingest/pony.jpg", "http://digaponyportland.com"),
-  ("Here After", "images/ingest/here.jpg", "http://hereafterpdx.com/"),
-  ("Victoria Bar", "images/ingest/victoria.jpg", "http://victoriapdx.com"),
-  ("Quality Bar", "images/ingest/quality.jpg", "http://www.qualitybarpdx.com/"),
+  ("Pearl Tavern", "images/ingest/pearl.jpg","http://pearltavernpdx.com", "231 NW 11th Ave, Portland, OR 97209"),
+  ("The Old Portland", "images/ingest/old.jpg", "http://pearltavernpdx.com", "1433 NW Quimby St, Portland, OR 97209"),
+  ("Bar Casa Vale", "images/ingest/vale.jpg", "www.barcasavale.com", "215 SE 9th Ave, Portland, OR 97214"),
+  ("Century", "images/ingest/century.jpg", "http://centurybarpdx.com/", "930 SE Sandy Blvd, Portland, OR 97214"),
+  ("The Bye and Bye", "images/ingest/bye.jpg", "http://thebyeandbye.com", "1011 NE Alberta St, Portland, OR 97211"),
+  ("Capitol", "images/ingest/capitol.jpg", "http://capitolpdx.com/", "1440 NE Broadway St, Portland, OR 97232"),
+  ("Dig A Pony", "images/ingest/pony.jpg", "http://digaponyportland.com", "736 SE Grand Ave, Portland, OR 97214"),
+  ("Here After", "images/ingest/here.jpg", "http://hereafterpdx.com/", "3326 SE Belmont St, Portland, OR 97214"),
+  ("Victoria Bar", "images/ingest/victoria.jpg", "http://victoriapdx.com", "4835 N Albina Ave, Portland, OR 97217"),
+  ("Quality Bar", "images/ingest/quality.png", "http://www.qualitybarpdx.com/", "931 SW Oak St, Portland, OR 97205"),
 ];
