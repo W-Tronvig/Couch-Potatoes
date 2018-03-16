71eeece6868539182a25500bad599dce0f0dd77f
William Tronvig <william.tronvig@gmail.com>
2018-03-15 20:40:38 -0700

    commit before first merge with group

--- main_js/outside.js
+++ main_js/outside.js
@@ -1,1 +1,36 @@
-Test
+
+var outsideLocations = function (name, image, website, address) {
+    this.name = name;
+    this.image = image; 
+    this.website = website;
+    this.address = address;
+}
+
+var outsideActiveArray = [];
+outsideActiveArray.push(
+new.outsideLocations("Forest Park", "main_images/ForestPark.jpg", "https://www.travelportland.com/article/forest-park/", "Portland, OR 97231"),
+new.outsideLocations("Aspen Lakes Golf Course", "main_images/GolfCourse.jpg", "https://www.aspenlakes.com/", "Aspen Lakes Golf 16900 Aspen Lakes Dr. Sisters, Oregon 97759"),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+);
+
+var outsideCalmArray = [];
+outsideCalmArray.push(
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+new.outsideLocations(),
+);
+
