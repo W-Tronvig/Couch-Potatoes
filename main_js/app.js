25bc509ad79390caca05a6a912b2e6d71d8dfc77
pereznancy <nan.per9@outlook.com>
2018-03-25 10:45:24 -0700

    done

--- main_js/app.js
+++ main_js/app.js
@@ -75,7 +75,7 @@ var outsideActiveArray = [
     new Activity("Wildwood Adventures", "../main_images/Outside/WildWoodTours.jpeg", "https://www.wildwoodtours.com/tours/columbia-gorge-tours/", "Portland, OR"),
     new Activity("Trackers Portland", "../main_images/Outside/Archery.jpg", "https://trackerspdx.com/archery.php", "Trackers Earth Portland 4617 SE Milwaukie Avenue Portland, OR 97202"),
     new Activity("Fly Style Paragliding", "../main_images/Outside/Paragliding.jpg", "http://www.flystyleparagliding.com", "Hood River, OR"),
-    new Activity("iFly!", "../main_images/Outside/IFly.jpeg", "https://www.iflyworld.com/?keyword=Ifly%20portland&MatchType=e&creative=220959825278&Placement=&gclid=EAIaIQobChMIq9DU7tTv2QIV1uDICh1nawdQEAAYASAAEgJS6_D_BwE", "10645 SW Greenburg Rd Tigard, OR 97223"),
+    new Activity("iFly!", "../main_images/Outside/IFly.jpeg", "https://www.iflyworld.com", "10645 SW Greenburg Rd Tigard, OR 97223"),
 ];
 
 var outsideCalmArray = [
@@ -200,7 +200,11 @@ var category = JSON.parse(localStorage.getItem("category"));
 var subCategory = JSON.parse(localStorage.getItem("subCategory"));
 console.log(category);
 console.log(subCategory);
-var chosenArray = activities[category][subCategory];
+if (category == null || subCategory == null) {
+  var chosenArray = [];
+}else {
+  var chosenArray = activities[category][subCategory];
+}
 //function will choose random activity
 
 
@@ -355,7 +359,7 @@ function saveAvatar(event) {
         avatarPics.appendChild(img);
     }
 }
-
+console.log(avatarPics);
 avatarPics.addEventListener("click", saveAvatar);
 
 function getLocalStorage() {
@@ -364,4 +368,3 @@ function getLocalStorage() {
     var getAvatar = localStorage.getItem('avatar');
     document.getElementById("showAvatar").innerHTML = getName + "<br/><img src=" + getAvatar + ">";
 }
-
